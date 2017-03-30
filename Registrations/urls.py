f081e3545f8232f5e2907a2f8cdafa7a1519a40c
Deepak Kumar Kar <kar.deepak2011@gmail.com>
2017-03-30 17:49:31 +0530

    staff member added

--- Registrations/urls.py
+++ Registrations/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from views import RegisterView
+from views import *
 
 urlpatterns = [
     url(r'^$', RegisterView.as_view(), name="home"),
+    url(r'^excel/$', ParticipantExcel, name="excel"),
 ]
--- Registrations/views.py
+++ Registrations/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.admin.views.decorators import staff_member_required
 
 from models import Participant
+
+import xlsxwriter
+from time import strftime, gmtime
+try:
+	import cStringIO as StringIO 
+except:
+	import StringIO
 # Create your views here.
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -28,3 +36,35 @@ class RegisterView(CreateView):
 		except:
 			res = super(RegisterView, self).post(request, *args, **kwargs)
 			return JsonResponse({'success' : 1, 'count' : Participant.objects.all().count()})
+
+@staff_member_required
+def ParticipantExcel(request, **kwargs):
+	entries = Participant.objects.filter(**kwargs).order_by('idno')
+	output = StringIO.StringIO()
+	workbook = xlsxwriter.Workbook(output)
+	worksheet = workbook.add_worksheet('new-spreadsheet')
+	date_format = workbook.add_format({'num_format': 'mmmm d yyyy'})
+	worksheet.write(0, 0, "Generated:")
+	generated = strftime("%d-%m-%Y %H:%M:%S UTC", gmtime())
+	worksheet.write(0, 1, generated)
+
+	worksheet.write(1, 0, "ID NO")
+	worksheet.write(1, 1, "Name")
+	worksheet.write(1, 2, "Institute")
+	worksheet.write(1, 3, "Phone")
+	worksheet.write(1, 4, "Can Solve?")
+
+	for i, p in enumerate(entries):
+		worksheet.write(i+2, 0, p.idno)
+		worksheet.write(i+2, 1, p.name)
+		worksheet.write(i+2, 2, p.institute)
+		worksheet.write(i+2, 3, p.phone)
+		worksheet.write(i+2, 4, p.can_solve)
+
+	workbook.close()
+	filename = 'Participants.xlsx'
+	output.seek(0)
+	response = HttpResponse(output.read(), content_type="application/ms-excel")
+	response['Content-Disposition'] = 'attachment; filename=%s' % filename
+	return response
+
