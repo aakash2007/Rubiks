713d9fcfb79be42b566d207436dc83c610943004
Deepak Kumar Kar <kar.deepak2011@gmail.com>
2017-04-05 00:34:09 +0530

    email content final

--- script.py
+++ script.py
@@ -4,15 +4,14 @@ def MassMail(filename):
 	body = """
 Hey !
 Congratulations !
-You have been registered for the official attempt to set a new Guinness World Record for the Highest Number of People to Solve the Rubik's Cube Simultaneously to be held on Sunday, 16th April '17.
-We will distribute the Cubes today i.e. the 4th of April '17, at your respective Bhawans.
+You have been registered for the official attempt to set a new Guinness World Record for Most Number of People solving Rubik's Cube to be held on Sunday, 16th April '17 at Gliders Club.
+We will distribute the Cubes today and tomorrow i.e. the 4th and 5th April, to your rooms.
 
-This is your opportunity to start hands-on learning and practice their solution. 
-You are strictly advised to be gentle with the practicing and take care not to damage the cubes. 
+This is your opportunity to start hands-on learning and practice their solution. Please take note of the fact that The cubes that we are providing have one face colour Black instead of White. You are strictly advised to be gentle with the practicing and take care not to damage the cubes.
 You can go to www.bits-wrs.ml/resources/ for tutorials.
 
 For more information, contact-
-Shivam 
+Shivam
 +91-8003322884
 Abhishek
 +91-9453212629
