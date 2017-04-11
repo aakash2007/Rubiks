f3f60cb503a35b7a4a270b23bde464bd256f736e
Deepak Kumar Kar <kar.deepak2011@gmail.com>
2017-04-12 03:08:02 +0530

    bits excel

--- Registrations/urls.py
+++ Registrations/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^excel/$', ParticipantExcel, name="excel"),
     url(r'^excel/(?P<hostel>(.*))$', HostelExcel),
     url(r'^custom/$', CustomExcel),
-    url(r'^btis/$', BITSExcel),
+    url(r'^bits/$', BITSExcel),
     url(r'^resources/$', TemplateView.as_view(template_name='resources.html'), name="resources"),
 ]
