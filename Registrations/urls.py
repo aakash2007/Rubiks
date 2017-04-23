24373173dcf28288f5ae5a946659466dfd4be62c
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-04-23 16:26:54 +0530

    Removed URLs

--- Registrations/urls.py
+++ Registrations/urls.py
@@ -7,12 +7,12 @@ import barcode
 
 urlpatterns = [
     url(r'^$', RegisterView.as_view(), name="home"),
-    url(r'^excel/$', ParticipantExcel, name="excel"),
-    url(r'^excel/(?P<hostel>(.*))$', HostelExcel),
-    url(r'^custom/$', CustomExcel),
-    url(r'^bits/$', BITSExcel),
+    # url(r'^excel/$', ParticipantExcel, name="excel"),
+    # url(r'^excel/(?P<hostel>(.*))$', HostelExcel),
+    # url(r'^custom/$', CustomExcel),
+    # url(r'^bits/$', BITSExcel),
     url(r'^resources/$', TemplateView.as_view(template_name='resources.html'), name="resources"),
-    url(r'^barcode/$', barcode.barcode, name="barcode"),
-    url(r'^html/$', barcode.htmls, name="barcode"),
+    # url(r'^barcode/$', barcode.barcode, name="barcode"),
+    # url(r'^html/$', barcode.htmls, name="barcode"),
     url(r'^timer/$', TemplateView.as_view(template_name='timer.html')),
 ]
